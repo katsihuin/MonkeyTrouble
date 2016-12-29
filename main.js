e124885d5874e9684ef86568443cc49ac322278b
katsihuin <katsihuin@gmail.com>
2016-12-28 22:56:35 -0500

    ejercicio terminado

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 // Codigo para consultar si estas en problemas o no 
 function consultar()
 {
-    var monkeyA = document.getElementById("monkeyA");
-    var monkeyB = document.getElementById("monkeyB");
+    var monkeyA = document.getElementById("monkeyA").checked;
+    var monkeyB = document.getElementById("monkeyB").checked;
     var aSmile = document.getElementById("si");
     var bSmile = document.getElementById("no");
     var salida = document.getElementById("salida");
 
-    if (monkeyA.checked && monkeyB.checked && aSmile.checked){
+    if (monkeyA == "" && monkeyB == ""){
+        document.getElementById("salida").innerHTML = "<div class='alert'>Seleccione un Mono</div>";
+    }
+    else if (monkeyA && monkeyB && aSmile.checked){
         //Estas en problemas
         salida.innerHTML = "Si";
     } 
-    else if (monkeyA.checked && monkeyB.checked && bSmile.checked){
+    else if (monkeyA && monkeyB && bSmile.checked){
         //Estas en problemas
         salida.innerHTML = "Si";
     } 
